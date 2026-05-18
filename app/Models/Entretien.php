9580d591d5d90a7995e822de6a3c79d1a79227aa
hassan_aftah <aftahhassan25@gmail.com>
2026-05-18 13:09:43 +0100

    models

--- app/Models/Entretien.php
+++ app/Models/Entretien.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entretien extends Model
 {
-    //
+    protected $fillable = [
+        'candidature_id',
+        'type',
+        'date_heure',
+        'notes_preparation',
+        'resultat',
+    ];
+
+    // Un entretien appartient à une candidature
+    public function candidature()
+    {
+        return $this->belongsTo(Candidature::class);
+    }
 }
